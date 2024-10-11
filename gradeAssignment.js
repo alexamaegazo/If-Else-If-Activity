625fb97dd1490c563bf9d7579c3ed829291a2493
alexamaegazo <alexamaegazo5@gmail.com>
2024-10-11 12:13:04 +0800

    Initial commit with gradeAssignment.js

---
+++ gradeAssignment.js
@@ -0,0 +1,16 @@
+//Prompt the user to enter their grade
+
+function askGrade(){
+    let grade = prompt("Please enter your grade:");
+    if (grade >= 90 && grade <= 100) {
+        alert("Excellent!");
+    } else if (grade >= 80 && grade <= 89) {
+        alert("Good!");
+    } else if (grade >= 70 && grade <= 79 ) { 
+        alert("Fair!");
+    }else if (grade <= 70) {
+        alert("Needs Improvement!");
+    }else {
+        alert("Invalid Input");
+    }   
+}
